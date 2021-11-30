53a130dac7e019b1ce2c63adb0ad63acb09d433d
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-30 17:00:40 -0600

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-const wrapper = document.querySelector('.items')
+const wrapper = document.querySelector('.items');
 const items = [
   {
     description: 'wash the dishes',
@@ -12,37 +12,35 @@ const items = [
     complete: false,
     index: '1',
   },
-]
+];
 
 function documentToDo() {
-    for (let i = 0; i < items.length; i += 1) {
-      const frag = document.createDocumentFragment();
-      console.log(frag);
-      const todoItem = items[i];
-      console.log(i, todoItem);
+  for (let i = 0; i < items.length; i += 1) {
+    const frag = document.createDocumentFragment();
+    const todoItem = items[i];
 
-      const todoList = document.createElement('li');
-      const todoDiv = document.createElement('div');
-      const checkBox = document.createElement('input');
-      const labelDesc = document.createElement('label');
-      const span = document.createElement('span')
+    const todoList = document.createElement('li');
+    const todoDiv = document.createElement('div');
+    const checkBox = document.createElement('input');
+    const labelDesc = document.createElement('label');
+    const span = document.createElement('span');
 
-      todoDiv.classList.add('todo', 'input');
-      todoDiv.appendChild(checkBox);
-      checkBox.type = 'checkbox';
-      checkBox.name = todoItem.index;
-      checkBox.id = todoItem.index;
-      checkBox.checked = todoItem.complete;
+    todoDiv.classList.add('todo', 'input');
+    todoDiv.appendChild(checkBox);
+    checkBox.type = 'checkbox';
+    checkBox.name = todoItem.index;
+    checkBox.id = todoItem.index;
+    checkBox.checked = todoItem.complete;
 
-      todoDiv.appendChild(labelDesc);
-      labelDesc.htmlFor = todoItem.index;
-      labelDesc.textContent = todoItem.description;
+    todoDiv.appendChild(labelDesc);
+    labelDesc.htmlFor = todoItem.index;
+    labelDesc.textContent = todoItem.description;
 
-      todoList.appendChild(todoDiv);
-      todoList.appendChild(span);
-      span.innerHTML = '&#8942;';
-      frag.appendChild(todoList);
-      wrapper.appendChild(frag);
+    todoList.appendChild(todoDiv);
+    todoList.appendChild(span);
+    span.innerHTML = '&#8942;';
+    frag.appendChild(todoList);
+    wrapper.appendChild(frag);
   }
 }
 
