cec648424d57fcca557b159d4e280535c804c080
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-02 20:27:03 -0600

    Remove reindexing on check

--- src/complete.js
+++ src/complete.js
@@ -1,33 +1,28 @@
 export const itemComplete = (item, items) => {
   item.complete = true;
   const indexGreater = items.filter((i) => i.index > item.index);
-  // console.log(items.filter(i => i.index > item.index));
   for (let i = 0; i < indexGreater.length; i += 1) {
     if (!indexGreater[i].complete) {
-      // console.log('iter incomp index greater', indexGreater[i]);
       indexGreater[i].index -= 1;
       item.index += 1;
     }
   }
-  // console.log('finish', items);
 };
 
 export const itemIncomplete = (item, items) => {
   item.complete = false;
   const indexLesser = items.filter((i) => i.index < item.index);
   for (let i = 0; i < indexLesser.length; i += 1) {
-    // console.log('incomp iter index less', indexLesser[i]);
     indexLesser[i].index += 1;
   }
   item.index = 0;
-  // console.log('finish', items);
 };
 
 export const toggleComplete = (item, items) => {
   const itemElement = document.getElementById(item.index);
   if (itemElement.checked) {
-    itemComplete(item, items);
+    item.complete = true;
   } else {
-    itemIncomplete(item, items);
+    item.complete = false;
   }
 };
